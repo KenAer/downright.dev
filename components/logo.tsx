246c7807caa20960e7b7d11c75d56206813a8473
KenAer <kenaer@gmail.com>
2022-06-19 11:03:47 -0400

    added social icons

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,8 +1,4 @@
-type Props = {
-    color: string;
-}
-
-export default function Logo(params: Props) {
+export default function Logo() {
     return (
         <div className="p-4 text-center">
             <h1 className="text-neutral-400 font-serif text-3xl">&#123;downrightCreative&#125;</h1>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,19 @@ import { ReactElement } from "react"
 import Layout from "../components/layout"
 import Logo from "../components/logo"
 import type { NextPageWithLayout } from "./_app"
+import EmailIcon from "../components/icons/email"
+import TwitterIcon from "../components/icons/twitter"
 
 const IndexPage: NextPageWithLayout = () => {
     return (
       <>
-        <Logo color={"white"} />
+        <Logo />
+        <div className="flex justify-center text-neutral-400">
+            <div className="grid grid-flow-col gap-4">
+                <EmailIcon />
+                <TwitterIcon />
+            </div>    
+        </div>
       </>
     )
 }
